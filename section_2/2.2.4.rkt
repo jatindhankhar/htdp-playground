042628b97f2bf72eb1c96cd5112eb17b235e29f6
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2015-05-15 18:35:57 +0530

    Did some exercises from section 2.1 to 2.2

---
+++ section_2/2.2.4.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 2.2.4) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "convert.rkt" "teachpack" "htdp")))))
+(define (convert3 first second third)
+  ( + (* third 100) (* second 10) first)
+  )
+(convert3 1 2 3)
