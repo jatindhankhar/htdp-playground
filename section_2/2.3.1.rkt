5878155e7c0a92619ed8a5cfe2b220be99117bf8
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2015-05-16 20:22:17 +0530

    Completed the section 2 , except error sub-part

---
+++ section_2/2.3.1.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 2.3.1) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "convert.rkt" "teachpack" "htdp")))))
+(define (tax gross_pay)
+  ( * (/ 100 15) gross_pay)
+  )
+
+(define (netpay hours)
+  ( * hours 12)
+  )
+  
---
+++ section_2/2.3.2.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 2.3.2) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "convert.rkt" "teachpack" "htdp")))))
+(define (sum-coins pennies nickels dimes quarters)
+  (+ (* 1 pennies) (* 5 nickels) (* 10 dimes) (* 25 quarters)
+     ))
